f972f30ef57b2ea54457b5347a02a8f0a2e2b499
Vittorio Zaidman <ogd2k5@users.noreply.github.com>
2015-03-15 16:12:59 +0200

    setting new design

--- src/pixelr.c
+++ src/pixelr.c
@@ -6,12 +6,13 @@ Window *window;
 TextLayer *hours_layer;
 TextLayer *mins_layer;
 TextLayer *secs_layer;
-TextLayer *day_layer;
-TextLayer *month_layer;
-TextLayer *year_layer;
+TextLayer *date_layer;
 TextLayer *battery_layer;
-static GFont *PixelFont34;
-static GFont *MiniPixelFont10;
+static GFont *PixelFont48;
+static GFont *PixelFont40;
+static GFont *PixelFont32;
+static GFont *PixelFont18;
+static GFont *PixelFont10;
 static GBitmap *bluetooth_connected_image;
 static GBitmap *bluetooth_disconnected_image;
 static BitmapLayer *bluetooth_layer;
@@ -27,56 +28,45 @@ static void do_init(void)
 	window_stack_push(window, true);
 	window_set_background_color(window, GColorBlack);
 	Layer *root_layer = window_get_root_layer(window);
-	PixelFont34 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_34));
-	MiniPixelFont10 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MINI_PIXEL_10));
+	PixelFont48 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_48));
+	PixelFont40 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_40));
+	PixelFont32 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_32));
+	PixelFont18 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_18));
+	PixelFont10= fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_FONT_10));
 
 	// Hours layer
-	hours_layer = text_layer_create(GRect(5, 40, 40 /* width */, 37 /* height */));
+	hours_layer = text_layer_create(GRect(40, 0, 65 /* width */, 55 /* height */));
 	text_layer_set_text_color(hours_layer, GColorWhite);
-	text_layer_set_background_color(hours_layer, GColorClear);
-	text_layer_set_font(hours_layer, PixelFont34);
+	text_layer_set_background_color(hours_layer, GColorBlack);
+	text_layer_set_font(hours_layer, PixelFont48);
 	text_layer_set_text_alignment(hours_layer, GTextAlignmentCenter);
-
+	
 	// Minutes layer
-	mins_layer = text_layer_create(GRect(5, 85, 40 /* width */, 37 /* height */));
+	mins_layer = text_layer_create(GRect(49, 47, 40 /* width */, 37 /* height */));
 	text_layer_set_text_color(mins_layer, GColorWhite);
-	text_layer_set_background_color(mins_layer, GColorClear);
-	text_layer_set_font(mins_layer, PixelFont34);
+	text_layer_set_background_color(mins_layer, GColorBlack);
+	text_layer_set_font(mins_layer, PixelFont40);
 	text_layer_set_text_alignment(mins_layer, GTextAlignmentCenter);
 	
 	// Seconds layer
-	secs_layer = text_layer_create(GRect(5, 130, 40 /* width */, 37 /* height */));
+	secs_layer = text_layer_create(GRect(49, 87, 40 /* width */, 37 /* height */));
 	text_layer_set_text_color(secs_layer, GColorWhite);
-	text_layer_set_background_color(secs_layer, GColorClear);
-	text_layer_set_font(secs_layer, PixelFont34);
+	text_layer_set_background_color(secs_layer, GColorBlack);
+	text_layer_set_font(secs_layer, PixelFont32);
 	text_layer_set_text_alignment(secs_layer, GTextAlignmentCenter);
 
-	// Day layer
-	day_layer = text_layer_create(GRect(105, 40, 40 /* width */, 37 /* height */));
-	text_layer_set_text_color(day_layer, GColorWhite);
-	text_layer_set_background_color(day_layer, GColorClear);
-	text_layer_set_font(day_layer, PixelFont34);
-	text_layer_set_text_alignment(day_layer, GTextAlignmentCenter);
-
-	// Month layer
-	month_layer = text_layer_create(GRect(105, 85, 40 /* width */, 37 /* height */));
-	text_layer_set_text_color(month_layer, GColorWhite);
-	text_layer_set_background_color(month_layer, GColorClear);
-	text_layer_set_font(month_layer, PixelFont34);
-	text_layer_set_text_alignment(month_layer, GTextAlignmentCenter);
-
-	// Year layer
-	year_layer = text_layer_create(GRect(105, 130, 40 /* width */, 37 /* height */));
-	text_layer_set_text_color(year_layer, GColorWhite);
-	text_layer_set_background_color(year_layer, GColorClear);
-	text_layer_set_font(year_layer, PixelFont34);
-	text_layer_set_text_alignment(year_layer, GTextAlignmentCenter);
+	// Date layer
+	date_layer = text_layer_create(GRect(20, 140, 105 /* width */, 25 /* height */));
+	text_layer_set_text_color(date_layer, GColorWhite);
+	text_layer_set_background_color(date_layer, GColorBlack);
+	text_layer_set_font(date_layer, PixelFont18);
+	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
 	
 	// Battery status layer
 	battery_layer = text_layer_create(GRect(110, 3, 35 /* width */, 20 /* height */));
 	text_layer_set_text_color(battery_layer, GColorWhite);
-	text_layer_set_background_color(battery_layer, GColorClear);
-	text_layer_set_font(battery_layer, MiniPixelFont10);
+	text_layer_set_background_color(battery_layer, GColorBlack);
+	text_layer_set_font(battery_layer, PixelFont10);
 	text_layer_set_text_alignment(battery_layer, GTextAlignmentCenter);
 	handle_battery(battery_state_service_peek());
 	//text_layer_set_text(battery_layer, "100%");
@@ -97,11 +87,9 @@ static void do_init(void)
 	bluetooth_connection_service_subscribe(&handle_bluetooth);
 
 	layer_add_child(root_layer, text_layer_get_layer(hours_layer));
-	layer_add_child(root_layer, text_layer_get_layer(mins_layer));
-	layer_add_child(root_layer, text_layer_get_layer(secs_layer));
-	layer_add_child(root_layer, text_layer_get_layer(day_layer));
-	layer_add_child(root_layer, text_layer_get_layer(month_layer));
-	layer_add_child(root_layer, text_layer_get_layer(year_layer));
+	//layer_add_child(root_layer, text_layer_get_layer(mins_layer));
+	//layer_add_child(root_layer, text_layer_get_layer(secs_layer));
+	layer_add_child(root_layer, text_layer_get_layer(date_layer));
 	layer_add_child(root_layer, bitmap_layer_get_layer(bluetooth_layer));
 	layer_add_child(root_layer, text_layer_get_layer(battery_layer));
 }
@@ -111,13 +99,15 @@ static void do_deinit(void)
 	tick_timer_service_unsubscribe();
 	battery_state_service_unsubscribe();
 	bluetooth_connection_service_unsubscribe();
-	fonts_unload_custom_font(PixelFont34);
+	fonts_unload_custom_font(PixelFont48);
+	fonts_unload_custom_font(PixelFont40);
+	fonts_unload_custom_font(PixelFont32);
+	fonts_unload_custom_font(PixelFont18);
+	fonts_unload_custom_font(PixelFont10);
 	text_layer_destroy(hours_layer);
 	text_layer_destroy(mins_layer);
 	text_layer_destroy(secs_layer);
-	text_layer_destroy(day_layer);
-	text_layer_destroy(month_layer);
-	text_layer_destroy(year_layer);
+	text_layer_destroy(date_layer);
 	bitmap_layer_destroy(bluetooth_layer);
 	text_layer_destroy(battery_layer);
 	window_destroy(window);
@@ -129,7 +119,14 @@ static void handle_battery(BatteryChargeState charge_state)
 	
 	if (charge_state.is_charging)
 	{
-		snprintf(battery_text, sizeof(battery_text), "ch");
+		if (charge_state.charge_percent >= 98)
+		{
+			snprintf(battery_text, sizeof(battery_text), "Full");
+		}
+		else
+		{
+			snprintf(battery_text, sizeof(battery_text), "...");
+		}
 	}
 	else
 	{
@@ -147,27 +144,23 @@ static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed)
 	static char hours[] = "00"; // Needs to be static because it's used by the system later.
 	static char mins[] = "00"; // Needs to be static because it's used by the system later.
 	static char secs[] = "00"; // Needs to be static because it's used by the system later.
-	static char day[] = "00"; // Needs to be static because it's used by the system later.
-	static char month[] = "00"; // Needs to be static because it's used by the system later.
-	static char year[] = "00"; // Needs to be static because it's used by the system later.
+	static char date[] = "00-00-00"; // Needs to be static because it's used by the system later.
 
-	strftime(hours, sizeof(hours), "%H", tick_time);
+	//strftime(hours, sizeof(hours), "%H", tick_time);
+	snprintf(hours, sizeof(hours), "88");
 	text_layer_set_text(hours_layer, hours);
 
-	strftime(mins, sizeof(mins), "%M", tick_time);
+	//strftime(mins, sizeof(mins), "%M", tick_time);
+	snprintf(mins, sizeof(mins), "88");
 	text_layer_set_text(mins_layer, mins);
 
-	strftime(secs, sizeof(secs), "%S", tick_time);
+	//strftime(secs, sizeof(secs), "%S", tick_time);
+	snprintf(secs, sizeof(secs), "88");
 	text_layer_set_text(secs_layer, secs);
 
-	strftime(day, sizeof(day), "%d", tick_time);
-	text_layer_set_text(day_layer, day);
-
-	strftime(month, sizeof(month), "%m", tick_time);
-	text_layer_set_text(month_layer, month);	
-
-	strftime(year, sizeof(year), "%y", tick_time);
-	text_layer_set_text(year_layer, year);
+	//strftime(date, sizeof(date), "%d-%m-%y", tick_time);
+	snprintf(date, sizeof(date), "88-88-88");
+	text_layer_set_text(date_layer, date);
 
 	// Samples the battery status every 50 minutes
 	curr_mins = (*tick_time).tm_min;
